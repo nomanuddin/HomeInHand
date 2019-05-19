e56d840ccff56d1843e9c470848c493c18c1edad
Noman <noman.uddin@live.com>
2019-05-19 03:59:28 +0200

    With onOff buttons

--- app/src/main/java/com/homeinhand/MainActivity.java
+++ app/src/main/java/com/homeinhand/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,6 +17,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.concurrent.ExecutionException;
 
+import static java.lang.Boolean.FALSE;
+
 /*  To turn on Device connected to Relay one:
     http://192.168.0.18/?param1=1&param2=1*/
 
@@ -23,8 +26,35 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MyApp";
     private static Context context;
-    ImageView image;
     String serverUri = "http://192.168.0.18/";
+    boolean statusBulb1 = FALSE;
+    boolean statusBulb2 = FALSE;
+    boolean statusBulb3 = FALSE;
+    boolean statusBulb4 = FALSE;
+    boolean statusBulb5 = FALSE;
+    boolean statusBulb6 = FALSE;
+    boolean statusBulb7 = FALSE;
+    boolean statusBulb8 = FALSE;
+
+
+    ImageView image1;
+    ImageView image2;
+    ImageView image3;
+    ImageView image4;
+    ImageView image5;
+    ImageView image6;
+    ImageView image7;
+    ImageView image8;
+
+    ImageButton imageButton1;
+    ImageButton imageButton2;
+    ImageButton imageButton3;
+    ImageButton imageButton4;
+    ImageButton imageButton5;
+    ImageButton imageButton6;
+    ImageButton imageButton7;
+    ImageButton imageButton8;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +62,136 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
 
-        image = (ImageView) findViewById(R.id.imV_Bulb1);
+        image1 = (ImageView) findViewById(R.id.imageView1);
+        image2 = (ImageView) findViewById(R.id.imageView2);
+        image3 = (ImageView) findViewById(R.id.imageView3);
+        image4 = (ImageView) findViewById(R.id.imageView4);
+        image5 = (ImageView) findViewById(R.id.imageView5);
+        image6 = (ImageView) findViewById(R.id.imageView6);
+        image7 = (ImageView) findViewById(R.id.imageView7);
+        image8 = (ImageView) findViewById(R.id.imageView8);
+
+        imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+        imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+        imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+        imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+        imageButton7 = (ImageButton) findViewById(R.id.imageButton7);
+        imageButton8 = (ImageButton) findViewById(R.id.imageButton8);
     }
 
     public static Context getAppContext() {
         return MainActivity.context;
+
     }
+
+    public void onClick(ImageButton imageButton) {
+
+        switch (imageButton.getId()) {
+            case R.id.imageButton1:
+                imageButton1 = (ImageButton) findViewById(R.id.imageButton1);
+                if (statusBulb1) {
+                    imageButton1.setImageResource(R.drawable.ic_off_button);
+                    statusBulb1 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton1.setImageResource(R.drawable.ic_on_button);
+                    statusBulb1 = true;
+                    trunBulb1On(view);
+                }
+            case R.id.imageButton2:
+                imageButton2 = (ImageButton) findViewById(R.id.imageButton2);
+                if (statusBulb2) {
+                    imageButton2.setImageResource(R.drawable.ic_off_button);
+                    statusBulb2 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton2.setImageResource(R.drawable.ic_on_button);
+                    statusBulb2 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton3:
+                imageButton3 = (ImageButton) findViewById(R.id.imageButton3);
+                if (statusBulb3) {
+                    imageButton3.setImageResource(R.drawable.ic_off_button);
+                    statusBulb3 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton3.setImageResource(R.drawable.ic_on_button);
+                    statusBulb3 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton4:
+                imageButton4 = (ImageButton) findViewById(R.id.imageButton4);
+                if (statusBulb4) {
+                    imageButton4.setImageResource(R.drawable.ic_off_button);
+                    statusBulb4 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton4.setImageResource(R.drawable.ic_on_button);
+                    statusBulb4 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton5:
+                imageButton5 = (ImageButton) findViewById(R.id.imageButton5);
+                if (statusBulb5) {
+                    imageButton5.setImageResource(R.drawable.ic_off_button);
+                    statusBulb5 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton5.setImageResource(R.drawable.ic_on_button);
+                    statusBulb5 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton6:
+                imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+                if (statusBulb6) {
+                    imageButton6.setImageResource(R.drawable.ic_off_button);
+                    statusBulb6 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton6.setImageResource(R.drawable.ic_on_button);
+                    statusBulb6 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton7:
+                imageButton7 = (ImageButton) findViewById(R.id.imageButton7);
+                if (statusBulb7) {
+                    imageButton7.setImageResource(R.drawable.ic_off_button);
+                    statusBulb7 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton7.setImageResource(R.drawable.ic_on_button);
+                    statusBulb7 = true;
+                    trunBulb1On(imageButton);
+                }
+            case R.id.imageButton8:
+                imageButton8 = (ImageButton) findViewById(R.id.imageButton8);
+                if (statusBulb8) {
+                    imageButton8.setImageResource(R.drawable.ic_off_button);
+                    statusBulb8 = false;
+                    trunBulb1Off(imageButton);
+                } else {
+                    imageButton8.setImageResource(R.drawable.ic_on_button);
+                    statusBulb8 = true;
+                    trunBulb1On(imageButton);
+                }
+        }
+
+    }
+
 
     /*  Function: trunBulb1On
         Description: This API will send the request to Client to turn the specific Bulb ON*/
-    public void trunBulb1On(View view)
-    {
-        Log.i(TAG, "trunBulb1On called" );
+    public void trunBulb1On(View view) {
+        Log.i(TAG, "trunBulb1On called");
         String ret = "";
-        ret=sendRequestToServer("1");
+        ret = sendRequestToServer("1");
 
-        if(ret!=null) {
+        if (ret != null) {
             if (ret.contains("OK")) {
                 /*Update image : BulbOff*/
-                image.setImageResource(R.drawable.ic_bulb_on);
             }
         }
 
@@ -58,16 +199,16 @@ public class MainActivity extends AppCompatActivity {
 
     /*  Function: trunBulb1Off
         Description: This API will send the request to Client to turn the specific Bulb OFF*/
-    public void trunBulb1Off(View view)
-    {
-        Log.i(TAG, "trunBulb1Off called" );
+    public void trunBulb1Off(View view) {
+        Log.i(TAG, "trunBulb1Off called");
         String ret = "";
-        ret=sendRequestToServer("0");
+        ret = sendRequestToServer("0");
 
-        if(ret!=null) {
+        if (ret != null) {
             if (ret.contains("OK")) {
                 /*Update image : BulbOff*/
-                image.setImageResource(R.drawable.ic_bulb_off);
+                //to do
+                //image.setImageResource(R.drawable.ic_bulb_off);
             }
         }
 
